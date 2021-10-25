1b6aeffc0726891804f357853001483ed5023611
catherine-tate <catherine21tate@gmail.com>
2021-10-25 16:09:49 -0400

    now it switches back and forth between configs

--- changeurself.py
+++ changeurself.py
@@ -3,6 +3,10 @@ print("configuration A")
 file = open(sys.argv[0], "r")
 lines = file.readlines()
 file.close()
-file = open("newFile.py", "x")
+if(lines[1][21] == "A"):
+	lines[1] = 'print("configuration B")\n'
+else:
+	lines[1] = 'print("configuration A")\n'
+file = open(sys.argv[0], "w")
 for line in lines:
 	file.write(line)
